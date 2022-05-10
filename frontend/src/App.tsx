a68a95c5f7a975837bf0c44a787d572ef2d6773b
DriRSantos <drirsantos@outlook.com>
2022-05-10 18:17:22 -0300

    Form Layout

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -7,7 +7,7 @@ import Listing from 'pages/Listing';
 import Form from 'pages/Form';
 import Navbar from "components/Navbar";
 
-function App() {
+function App(): JSX.Element {
   return (
     <BrowserRouter>
       <Navbar />
